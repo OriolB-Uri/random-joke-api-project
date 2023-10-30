1be55d98520710a1fd40936d5aded7b8bc4f653a
OriolB-Uri <oriolbotella@hotmail.com>
2023-10-30 14:04:45 +0100

    Acabado

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 const image = document.querySelector('img');
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
+
+
+button.addEventListener("click", async function(){
+    getRandomJoke();
+})
+
+async function getRandomJoke() {
+    const respuesta = await fetch("https://api.chucknorris.io/jokes/random");
+    const datos = await respuesta.json();
+    console.log(datos);
+    jokeDIV.textContent = datos.value;
+}
